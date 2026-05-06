197e2efbe0d30607b7cd4cb0b33fa8905c7c657f
Hyuko123 <gaylordlemene@gmail.com>
2026-05-06 22:56:10 +0200

    Update datebase.py

--- api/datebase.py
+++ api/datebase.py
@@ -1,33 +1,24 @@
-
+# api/database.py
 import json
 import os
+from .config import USERS_DEFAULT
 
-# Chemins vers tes fichiers JSON dans le dossier /data
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 VENTES_FILE = os.path.join(BASE_DIR, 'data', 'ventes.json')
 USERS_FILE = os.path.join(BASE_DIR, 'data', 'users.json')
 
-# --- FONCTIONS POUR LES VENTES ---
 def charger_ventes():
-    if not os.path.exists(VENTES_FILE):
-        return []
-    with open(VENTES_FILE, 'r') as f:
-        return json.load(f)
+    if not os.path.exists(VENTES_FILE): return []
+    with open(VENTES_FILE, 'r') as f: return json.load(f)
 
 def sauvegarder_ventes(ventes):
-    # Crée le dossier data s'il n'existe pas
     os.makedirs(os.path.dirname(VENTES_FILE), exist_ok=True)
-    with open(VENTES_FILE, 'w') as f:
-        json.dump(ventes, f, indent=4)
+    with open(VENTES_FILE, 'w') as f: json.dump(ventes, f, indent=4)
 
-# --- FONCTIONS POUR LES UTILISATEURS ---
 def charger_users():
-    if not os.path.exists(USERS_FILE):
-        return {} # Les users sont souvent un dictionnaire {}
-    with open(USERS_FILE, 'r') as f:
-        return json.load(f)
+    if not os.path.exists(USERS_FILE): return USERS_DEFAULT
+    with open(USERS_FILE, 'r') as f: return json.load(f)
 
 def sauvegarder_users(users):
     os.makedirs(os.path.dirname(USERS_FILE), exist_ok=True)
-    with open(USERS_FILE, 'w') as f:
-        json.dump(users, f, indent=4)
+    with open(USERS_FILE, 'w') as f: json.dump(users, f, indent=4)
